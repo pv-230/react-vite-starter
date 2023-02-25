c351531e65aaef93beffcdab44b51dabb369f14f
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-02-25 15:18:23 -0500

    Update main.jsx import groupings

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GlobalStyle from './common/global-styles';
 
+import GlobalStyle from './common/global-styles';
 import App from './components/app';
 import './reset.css';
 
